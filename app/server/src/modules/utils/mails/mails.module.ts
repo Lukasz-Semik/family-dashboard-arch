a77331f516f9dba068ff0d717e7b5a4c451ebdf2
djpluki <djpluki@gmail.com>
2019-10-05 00:06:39 +0200

    make small fixes

--- app/server/src/modules/utils/mails/mails.module.ts
+++ app/server/src/modules/utils/mails/mails.module.ts
@@ -1,11 +1,9 @@
 import { Module, Global } from '@nestjs/common';
 
 import { MailsService } from './mails.service';
-// import { EnvModule } from '../env';
 
 @Global()
 @Module({
-  // imports: [EnvModule],
   providers: [MailsService],
   exports: [MailsService],
 })
